0db42bbf60f01d7d89afcb897be423269adcbc8c
wagnel <wagnelcuello@gmail.com>
2019-02-19 21:09:25 -0400

    .

--- Practica6-4/Controllers/HomeController.cs
+++ Practica6-4/Controllers/HomeController.cs
@@ -13,12 +13,14 @@ namespace Practica6_4.Controllers
             return View();
         }
 
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
+
 
         public ActionResult Contact()
         {
